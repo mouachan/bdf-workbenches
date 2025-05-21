50190e4a4065f482710ff99a69f146c9db2ef58d
mouachan <mouachan@redhat.com>
2025-05-21 08:35:46 +0200

    based on RHOAI notebook

---
+++ c9s-python-3.11/install_packages.R
@@ -0,0 +1,16 @@
+# install_packages.R
+
+# Load or install 'remotes' package
+if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
+}
+
+# Install specific versions of packages
+remotes::install_version('Rcpp', '1.0.14')
+remotes::install_version('tidyverse', '2.0.0')
+remotes::install_version('tidymodels', '1.3.0')
+remotes::install_version('plumber', '1.3.0')
+remotes::install_version('vetiver', '0.2.5')
+remotes::install_version('devtools', '2.4.5')
+remotes::install_version('terra', '1.7-71')   # specify version as needed
+remotes::install_version('sf', '1.0-15')      # specify version as needed
